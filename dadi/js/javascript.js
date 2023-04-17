ba6d0987b425ea94c15a8237e7e11958ffe481f3
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-04-17 17:00:46 +0200

    add: completed dadi

--- dadi/js/javascript.js
+++ dadi/js/javascript.js
@@ -1,3 +1,29 @@
+const player  = Math.round(Math.random () * 6);
+console.log(player)
+
+const playerComputer = Math.round(Math.random() * 6);
+console.log(playerComputer)
+
+
+if (player > playerComputer){
+
+    console.log ('Il giocatore ha vinto')
+}
+
+else if(player < playerComputer){
+    console.log ('Il giocatore ha perso')
+}
+    
+    
+    
+else{
+    (player == playerComputer)
+    console.log ('Pareggio')
+
+}
+
+    
+
 
 
     
