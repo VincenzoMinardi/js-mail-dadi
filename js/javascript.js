2ffc643150a521289a681cb9fad9df1abf8081e8
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-04-17 14:53:48 +0200

    add:email

---
+++ js/javascript.js
@@ -0,0 +1,24 @@
+const emailElement = prompt('Inserisci email');
+
+const errorEmail = alert ('email non valida');
+
+const myArr =[
+    'vincenzominardi@live.it', 'pippopluto@gmail.com', 'gianlucaferrari@gmail.com', 'rosannalagrutta@gmail.com'];
+    console.log(myArr);
+
+for (let i = 1; i < myArr.length; i++);{
+   
+    
+        if (emailElement ){
+            console.log (emailElement);
+        }
+        else if(emailElement){
+            console.log (errorEmail);
+            
+        }
+}
+
+
+    
+    
+
